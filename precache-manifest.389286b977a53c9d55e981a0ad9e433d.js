1ad85819495ffc16bed31bffd3ff02a4b562e449
Saber Pourrahimi <saber@Sabers-MacBook-Pro.local>
2021-10-19 20:56:04 +0330

    Updates

---
+++ precache-manifest.389286b977a53c9d55e981a0ad9e433d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ae620ae47e9da3d30e5fcbc0cae0d1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "af3331c4f5cd7d034621",
+    "url": "/Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "961ba4a5279141a525c6",
+    "url": "/Portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "8dcd29e5208fd8243eca",
+    "url": "/Portfolio/static/css/main.980f73da.chunk.css"
+  },
+  {
+    "revision": "3e8339983d782e59f56e",
+    "url": "/Portfolio/static/js/2.daa7555d.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/Portfolio/static/js/2.daa7555d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3331c4f5cd7d034621",
+    "url": "/Portfolio/static/js/3.c3b2e169.chunk.js"
+  },
+  {
+    "revision": "961ba4a5279141a525c6",
+    "url": "/Portfolio/static/js/4.db297c9f.chunk.js"
+  },
+  {
+    "revision": "8dcd29e5208fd8243eca",
+    "url": "/Portfolio/static/js/main.eca30435.chunk.js"
+  },
+  {
+    "revision": "a851df9ca3fca16d7484",
+    "url": "/Portfolio/static/js/runtime-main.f794b882.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "aa652c204cfd6c6e1e875e4e66b2e99d",
+    "url": "/Portfolio/static/media/Utech-Academy.aa652c20.png"
+  },
+  {
+    "revision": "1a28a4c510d2b7deec14db12a094bf82",
+    "url": "/Portfolio/static/media/amirkabir.1a28a4c5.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "2a5dac678fabffa2b6ae832e0ce2c5b0",
+    "url": "/Portfolio/static/media/panoramic.2a5dac67.png"
+  },
+  {
+    "revision": "ca5e30b3ab9c02ca9c7fa1596802d455",
+    "url": "/Portfolio/static/media/redux.ca5e30b3.png"
+  },
+  {
+    "revision": "565566747ed5318a939008f77080c3ed",
+    "url": "/Portfolio/static/media/sampad.56556674.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
