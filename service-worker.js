2309063ab6491e7953f57cd0b257ca10c7f8a5f3
spr021 <spr021@users.noreply.github.com>
2021-11-01 12:30:48 +0000

    Deploying to gh-pages from  @ 226469eb0c33420373ffbe8d1062062fedf3988b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.782f332f0ade0264bd1534af8755e613.js"
+  "/Portfolio/precache-manifest.699048347bd192e16a664788359f7f70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
